7482953fa4d40c9bd67a4d1a968e0a1765c3293e
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-30 21:13:17 +0530

    Flip 1

---
+++ Program1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of times to flip the coin:");
+        string input = Console.ReadLine();
+        int numFlips;
+        if (!int.TryParse(input, out numFlips) || numFlips <= 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a positive integer.");
+            return;
+        }
+
+        Random random = new Random();
+        int headsCount = 0;
+        int tailsCount = 0;
+
+        for (int i = 0; i < numFlips; i++)
+        {
+            double flip = random.NextDouble();
+
+            if (flip < 0.5)
+            {
+                tailsCount++;
+            }
+            else
+            {
+                headsCount++;
+            }
+        }
+
+        double headsPercentage = (double)headsCount / numFlips * 100;
+        double tailsPercentage = (double)tailsCount / numFlips * 100;
+
+        Console.WriteLine($"Heads: {headsPercentage}%");
+        Console.WriteLine($"Tails: {tailsPercentage}%");
+    }
+}
